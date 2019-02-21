62dd982590a8b56cbe6562248fed0d62e9b48a5d
Harrison Garovi <garovih@gmail.com>
2019-02-21 10:50:34 +0900

    made progress on the reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,19 @@
 class ReviewsController < ApplicationController
-  def review
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    # we need a restaurant
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:new, :create)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
   # get 'restaurants/:id', to: 'restaurants#show', as: :show_restaurant
   # post 'restaurants', to: 'restaurants#create', as: :create_restaurant
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
